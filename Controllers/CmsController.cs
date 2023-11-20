5ef04feca9d86918d40b80beb90b6806c90112e4
s.kovalenko <s.kovalenko@hybrid.ai>
2023-11-20 17:56:53 +0300

    Add project files.

---
+++ Controllers/CmsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Piranha.AspNetCore.Services;
+using Piranha;
+using Piranha.Models;
+using eVilage.Models;
+
+namespace eVilage.Controllers;
+
+[ApiExplorerSettings(IgnoreApi = true)]
+public class CmsController : Controller
+{
+    private readonly IApi _api;
+    private readonly IModelLoader _loader;
+
+    public CmsController(IApi api, IModelLoader loader) 
+    { 
+        _api = api; 
+        _loader = loader;
+    }
+
+    /// <summary>
+    /// Gets the page with the given id.
+    /// </summary>
+    /// <param name="id">The unique page id</param>
+    /// <param name="draft">If a draft is requested</param>
+    [Route("page")]
+    public async Task<IActionResult> Page(Guid id, bool draft = false)
+    {
+        try
+        {
+            var model = await _loader.GetPageAsync<StandardPage>(id, HttpContext.User, draft);
+
+            return View(model);
+        }
+        catch (UnauthorizedAccessException)
+        {
+            return Unauthorized();
+        }
+    }
+
+}
---
+++ Models/StandardPage.cs
@@ -0,0 +1,10 @@
+﻿using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace eVilage.Models;
+
+[PageType(Title = "Standard page")]
+public class StandardPage : Page<StandardPage>
+{
+
+}
